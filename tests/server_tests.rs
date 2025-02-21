995570fc6f8255f37bf2cafd738dfe033cdeb2ef
Pieter Frenssen <pieter@frenssen.be>
2025-02-21 17:12:33 +0200

    Test the 'server status' command.

--- tests/server_tests.rs
+++ tests/server_tests.rs
@@ -1,20 +1,14 @@
 use std::process::Stdio;
 use std::sync::{Arc};
+use nix::sys::signal::{kill, Signal};
+use nix::unistd::Pid;
+use serial_test::serial;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::process::Command;
 use tokio::sync::Mutex;
 use tokio::sync::Notify;
-use nix::sys::signal::{kill, Signal};
-use nix::unistd::Pid;
 
-#[tokio::test]
-async fn test_stop_server_by_sending_sigint() {
-    // Find a free port in the range 8000-9000.
-    let port = port_check::free_local_port_in_range(8000..9000)
-        .expect("A free port should be found in the range 8000-9000.");
-    std::env::set_var("HEALTHCHECKER_SERVER_ADDRESS", "127.0.0.1");
-    std::env::set_var("HEALTHCHECKER_SERVER_PORT", port.to_string());
-
+async fn start_server() -> (tokio::process::Child, Arc<Mutex<tokio::io::Lines<BufReader<tokio::process::ChildStderr>>>>, Arc<Notify>) {
     // Run `cargo run -- server start` as a child process.
     let mut server = Command::new("cargo")
         .args(&["run", "--", "server", "start"])
@@ -42,20 +36,35 @@ async fn test_stop_server_by_sending_sigint() {
     });
     notify.notified().await;
 
-    // Server is up. Terminate the server by sending SIGINT signal.
+    (server, lines, notify)
+}
+
+async fn server_status() -> (tokio::process::Child, Arc<Mutex<tokio::io::Lines<BufReader<tokio::process::ChildStdout>>>>) {
+    // Run `cargo run -- server status` as a child process.
+    let mut status_command = Command::new("cargo")
+        .args(&["run", "--", "server", "status"])
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("The command to check the server status should spawn a child process.");
+
+    // Capture the server's log output on stderr.
+    let stderr = status_command.stdout.take().expect("Stdout output should be captured.");
+    let reader = BufReader::new(stderr);
+    let lines = Arc::new(Mutex::new(reader.lines()));
+
+    (status_command, lines)
+}
+
+async fn stop_server(server: &mut tokio::process::Child) {
     let pid = Pid::from_raw(server.id().expect("The server process should be running and have a process ID.") as i32);
     kill(pid, Signal::SIGINT).expect("The SIGINT signal should be sent.");
 
     // Wait for the server to shut down.
     let status = server.wait().await.expect("The server process should exit.");
     assert!(status.success(), "Server did not shut down gracefully");
+}
 
-    // Check that the log output contains the expected log messages related to the server shutdown.
-    let regex_expected_lines = vec![
-        ".*INFO.*Received SIGINT, shutting down.*",
-        ".*INFO.*Server stopped.*",
-    ];
-
+async fn check_log_output_regex(lines: Arc<Mutex<tokio::io::Lines<BufReader<tokio::process::ChildStderr>>>>, regex_expected_lines: Vec<&str>) {
     let mut captured_lines = Vec::new();
     while let Ok(Some(line)) = lines.lock().await.next_line().await {
         captured_lines.push(line);
@@ -66,5 +75,67 @@ async fn test_stop_server_by_sending_sigint() {
         let found = captured_lines.iter().any(|line| re.is_match(line.as_ref()));
         assert!(found, "The output contains the line '{}'.", expected_line);
     }
+}
 
+async fn check_log_output<T>(lines: Arc<Mutex<tokio::io::Lines<BufReader<T>>>>, expected_lines: Vec<&str>)
+where
+    T: tokio::io::AsyncRead + Unpin,
+{
+    let mut captured_lines = Vec::new();
+    while let Ok(Some(line)) = lines.lock().await.next_line().await {
+        captured_lines.push(line);
+    }
+
+    for expected_line in expected_lines {
+        let found = captured_lines.iter().any(|line| line == expected_line);
+        assert!(found, "The output contains the line '{}'.", expected_line);
+    }
+}
+
+#[tokio::test]
+#[serial]
+async fn test_stop_server_by_sending_sigint() {
+    let (mut server, lines, _notify) = start_server().await;
+
+    stop_server(&mut server).await;
+
+    let expected_lines = vec![
+        ".*INFO.*Received SIGINT, shutting down.*",
+        ".*INFO.*Server stopped.*",
+    ];
+    check_log_output_regex(lines, expected_lines).await;
+}
+
+#[tokio::test]
+#[serial]
+async fn test_server_status() {
+    // Initially `cargo run -- server status` should return `not running`.
+    let (mut status_command, lines) = server_status().await;
+    let expected_lines = vec!["not running"];
+    check_log_output(lines.clone(), expected_lines).await;
+
+    // The status command should exit with an error code.
+    let status = status_command.wait().await.expect("The status command should exit.");
+    assert!(!status.success(), "The status command should exit with an error code if the server is not running.");
+
+    // Start the server.
+    let (mut server, _, _notify) = start_server().await;
+
+    // Now the status command should return `running`.
+    let (mut status_command, lines) = server_status().await;
+    let expected_lines = vec!["running"];
+    check_log_output(lines.clone(), expected_lines).await;
+
+    // The status command should exit with a success code.
+    let status = status_command.wait().await.expect("The status command should exit.");
+    assert!(status.success(), "The status command should exit with a success code if the server is running.");
+
+    // Stop the server. The status command should return `not running` again.
+    stop_server(&mut server).await;
+
+    let (mut server_status, lines) = server_status().await;
+    let expected_lines = vec!["not running"];
+    check_log_output(lines.clone(), expected_lines).await;
+    let status = server_status.wait().await.expect("The status command should exit.");
+    assert!(!status.success(), "The status command should exit with an error code if the server is not running.");
 }
